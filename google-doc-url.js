5871b8bb25c322c4d93f9c1062f2086912457500
ix4 <38112035+ix4@users.noreply.github.com>
2019-07-13 15:20:27 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QK3LQNWwJRRezCc4LRsFoMfY0TrYKtP_iLMGB2wCemI/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wFf0jamoBmz2Jb5ae0co8Xbaz6F2pATNbYTtnsh8iMI/edit#gid=0';
